283bb37febd3a0aa9daedf9fef3f36d56d2ae1b4
MiguelHonda <miguelroshonda@gmail.com>
2025-02-05 11:59:14 +0100

    updated seconds2minutes method

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -10,9 +10,14 @@ namespace hada_p1
     {
         public static double Seconds2Minutes(double s)
         {
-  
-                return s / 60;
-            
+            if (s == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return s/60;
+            }
         }
     }
 }
