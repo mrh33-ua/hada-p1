a4c2635afd6a09623df514a7afa409672d136d7c
MiguelHonda <miguelroshonda@gmail.com>
2025-02-19 12:18:27 +0100

    added new funcionality

---
+++ hada-p1/Hada1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace hada_p1
+{
+    public static class HadaP1
+    {
+        // Convierte segundos a minutos
+        public static double SecondsToMinutes(double seconds)
+        {
+
+            if(seconds < 0) seconds = 0;
+            return seconds / 60;
+        }
+
+        // Convierte minutos a segundos
+        public static double MinutesToSeconds(double minutes)
+        {
+            return minutes * 60;
+        }
+    }
+}
